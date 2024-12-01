19e76b790cdb9f2d541bc7719b267ba1baf52637
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2024-12-01 09:11:06 +0000

    day 1 part 1 complete

--- src/day_1/main.rs
+++ src/day_1/main.rs
@@ -5,6 +5,27 @@ use std::fs;
 // build with: cargo build
 // run with  : cargo run -- ./src/day_1/intput.txt
 
+fn parse_two_columns(input: &str) -> Result<(Vec<i32>, Vec<i32>), Box<dyn std::error::Error>> {
+    let mut column1 = Vec::new();
+    let mut column2 = Vec::new();
+
+    for line in input.lines() {
+        let numbers: Vec<&str> = line.split_whitespace().collect();
+        if numbers.len() != 2 {
+            return Err(format!("Invalid line format: '{}'", line).into());
+        }
+
+        let num1: i32 = numbers[0].parse()?;
+        let num2: i32 = numbers[1].parse()?;
+        
+        column1.push(num1);
+        column2.push(num2);
+    }
+    column1.sort();
+    column2.sort();
+    Ok((column1, column2))
+}
+
 fn main() {
 
     // get the input file name from args
@@ -15,5 +36,24 @@ fn main() {
 
     let contents = fs::read_to_string(input_file).expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
+    match parse_two_columns(&contents) {
+        Ok((col1, col2)) => {
+            println!("Column 1: {:?}", col1);
+            println!("Column 2: {:?}", col2);
+            let col_1_size = col1.len();
+
+            let mut _sum = 0;
+            // now we have sorted col1 and col2 just find difference between pairs
+            for i in 0..col_1_size {
+                let _diff = col1[i] - col2[i];
+                println!("{}", _diff.abs());
+                _sum = _sum + _diff.abs();
+            }
+            println!("Sum is {}",_sum);
+        }
+        Err(e) => {
+            eprintln!("Error parsing input: {}", e);
+        }
+    }
+    //println!("With text:\n{contents}");
 }
