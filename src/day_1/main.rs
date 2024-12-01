d82f1ed41a926741f82b1e4b001a25f0d05aa88b
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2024-12-01 10:07:21 +0000

    day 1 part 2 done

--- src/day_1/main.rs
+++ src/day_1/main.rs
@@ -1,10 +1,11 @@
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 
 // advent of code 2024 : day 1
 // build with: cargo build
 // run with  : cargo run -- ./src/day_1/intput.txt
-
+ 
 fn parse_two_columns(input: &str) -> Result<(Vec<i32>, Vec<i32>), Box<dyn std::error::Error>> {
     let mut column1 = Vec::new();
     let mut column2 = Vec::new();
@@ -17,7 +18,7 @@ fn parse_two_columns(input: &str) -> Result<(Vec<i32>, Vec<i32>), Box<dyn std::e
 
         let num1: i32 = numbers[0].parse()?;
         let num2: i32 = numbers[1].parse()?;
-        
+
         column1.push(num1);
         column2.push(num2);
     }
@@ -27,10 +28,9 @@ fn parse_two_columns(input: &str) -> Result<(Vec<i32>, Vec<i32>), Box<dyn std::e
 }
 
 fn main() {
-
     // get the input file name from args
     let args: Vec<String> = env::args().collect();
-    let input_file:&String = &args[1];
+    let input_file: &String = &args[1];
 
     println!("input file = {input_file}");
 
@@ -38,22 +38,38 @@ fn main() {
 
     match parse_two_columns(&contents) {
         Ok((col1, col2)) => {
-            println!("Column 1: {:?}", col1);
-            println!("Column 2: {:?}", col2);
             let col_1_size = col1.len();
+            //println!("Column 1: {:?}\n", col1);
+            //println!("Column 2: {:?}\n", col2);
 
             let mut _sum = 0;
             // now we have sorted col1 and col2 just find difference between pairs
             for i in 0..col_1_size {
                 let _diff = col1[i] - col2[i];
-                println!("{}", _diff.abs());
                 _sum = _sum + _diff.abs();
             }
-            println!("Sum is {}",_sum);
+
+            println!("Day 1 part 1 answer, Sum is {}", _sum);
+
+                // Count matches and compute the product of counts
+            let mut match_counts = HashMap::new();
+            let mut total_product: usize = 0;
+            // part 2 has to find sum of the matched counts of numbers in second column
+
+            // Count matches
+            for &num in &col1 {
+                let count = col2.iter().filter(|&&x| x == num).count();
+                match_counts.insert(num, count);
+        
+                // Avoid multiplying by 0; if count is 0, total_product becomes 0
+                if count > 0 {
+                    total_product += count * (num as usize);
+                } 
+            }
+            println!("Day 1 part 2 answer, Sum is {}", total_product);
         }
         Err(e) => {
             eprintln!("Error parsing input: {}", e);
         }
     }
-    //println!("With text:\n{contents}");
 }
