b232d58a08500ffc5b5288a4846379ffd6a1c4e6
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2024-12-07 09:53:26 +0000

    nearer but not complete

--- src/day_6/main.rs
+++ src/day_6/main.rs
@@ -1,8 +1,8 @@
 use std::env;
+use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-
 // advent of code 2024 : day 6
 // build with: cargo build
 // run with  : cargo run --bin day_6 ./src/day_6/intput.txt
@@ -12,6 +12,17 @@ enum Direction {
     Down = 1,
     Left = 2,
     Right = 3,
+}
+
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Direction::Up => write!(f, "Up"),
+            Direction::Down => write!(f, "Down"),
+            Direction::Left => write!(f, "Left"),
+            Direction::Right => write!(f, "Right"),
+        }
+    }
 }
 
 #[derive(PartialEq)]
@@ -48,47 +59,125 @@ fn find_char_in_grid(grid: &[Vec<char>], target: char) -> Option<(usize, usize)>
     None // Return None if the character is not found
 }
 
-fn can_move(grid: &Vec<Vec<char>>, current_pos: (usize, usize), direction: Direction) -> bool {
-    let _ = direction;
-    let _ = current_pos;
-    let mut result: bool = true;
+fn can_move(grid: &Vec<Vec<char>>, current_pos: (usize, usize), direction: &Direction) -> bool {
+    let mut result: bool = false;
     //let width = grid[0][0].len();
-    let width = 0;
+    let width = grid.len();
     let height = grid[0].len();
 
-    println!("width={} height={}", width, height);
-
-    // compiler error - need to impl partialEq on Directions enum arrrrrr! (should have used C++ I'd have done this 3 hours ago)
-    if Direction::Up == direction {
-        result = current_pos.0 - 1 > 0;
-    } else if Direction::Down == direction {
-        result = current_pos.0 + 1 < height;
-    } else if Direction::Left == direction {
-        result = current_pos.0 - 1 > 0;
-    } else if Direction::Right == direction {
-        result = current_pos.1 + 1 < width;
+    // compile error - need to impl partialEq on Directions enum arrrrrr! (should have used C++ I'd have done this 3 hours ago)
+    match direction {
+        Direction::Up => {
+            println!("current_pos {:?}", current_pos);
+            if (current_pos.0 != 0) {
+                if current_pos.0 - 1 > 0 {
+                    result = true;
+                }
+            } else {
+                result = false;
+            }
+        }
+        Direction::Down => {
+            if current_pos.0 + 1 < height {
+                result = true;
+            }
+        }
+        Direction::Right => {
+            if current_pos.1 + 1 < width {
+                result = true;
+            }
+        }
+        Direction::Left => {
+            if current_pos.1 - 1 > 0 {
+                {
+                    result = true;
+                }
+            }
+        }
     }
+
     // return result
     result
 }
 
 fn move_to_next(
     grid: &Vec<Vec<char>>,
-    current_pos: (usize, usize),
-    direction: Direction,
+    current_pos: &(usize, usize),
+    mut direction: &Direction,
 ) -> Option<(usize, usize)> {
     let mut result: (usize, usize) = (0, 0);
 
-    if can_move(&grid, current_pos, direction) {
+    if can_move(&grid, *current_pos, &direction) {
         match direction {
-            Direction::Up => result = (current_pos.0 - 1, current_pos.1),
-            Direction::Down => result = (current_pos.0 + 1, current_pos.1),
-            Direction::Right => result = (current_pos.0, current_pos.1 + 1),
-            Direction::Left => result = (current_pos.0, current_pos.1 - 1),
+            Direction::Up => {
+                result = (current_pos.0 - 1, current_pos.1);
+                println!("direction={}", direction);
+                if result == (0, 0) {
+                    panic!("pos = 0 0");
+                    direction = &Direction::Right;
+                    println!("new direction={}", direction);
+                }
+            }
+            Direction::Down => {
+                result = (current_pos.0 + 1, current_pos.1);
+                println!("direction={}", direction);
+                if result == (0, 0) {
+                    println!("old direction={}", direction);
+                    direction = &Direction::Left;
+                    println!("new direction={}", direction);
+                }
+            }
+            Direction::Right => {
+                result = (current_pos.0, current_pos.1 + 1);
+                println!("direction={}", direction);
+                if result == (0, 0) {
+                    println!("old direction={}", direction);
+                    direction = &Direction::Down;
+                    println!("new direction={}", direction);
+                }
+            }
+            Direction::Left => {
+                result = (current_pos.0, current_pos.1 - 1);
+                println!("direction={}", direction);
+                if result == (0, 0) {
+                    println!("old direction={}", direction);
+                    direction = &Direction::Up;
+                    println!("new direction={}", direction);
+                }
+            }
         }
     } else {
-        println!("error from can_move");
+        match direction {
+            Direction::Up => {
+                result = (current_pos.0, current_pos.1 + 1);
+                println!("old direction={}", direction);
+                direction = &Direction::Right;
+                println!("new direction={}", direction);
+                //  panic!("pos = 0 0");
+            }
+            Direction::Down => {
+                result = (current_pos.0 - 1, current_pos.1);
+                println!("old direction={}", direction);
+                direction = &Direction::Left;
+                println!("new direction={}", direction);
+            }
+            Direction::Right => {
+                result = (current_pos.0, current_pos.1 + 1);
+                println!("old direction={}", direction);
+                direction = &Direction::Down;
+                println!("new direction={}", direction);
+            }
+            Direction::Left => {
+                result = (current_pos.0, current_pos.1 - 1);
+
+                println!("old direction={}", direction);
+                direction = &Direction::Up;
+                println!("new direction={}", direction);
+            }
+        }
     }
+    println!("horiz pos={} vert={}", current_pos.0, current_pos.1);
+
     Some(result)
 }
 
@@ -123,11 +212,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             let mut pos = (row, col);
 
-            let width_of_lab_space = grid[0].len(); // assuming every row is the same!
+            let mut total = 0;
 
-            let pos = move_to_next(&grid, pos, _direction);
+            while Some(pos).is_some() {
+                pos = move_to_next(&grid, &pos, &_direction).expect("done");
 
-            println!("New Position = {:?}", pos);
+                println!("New Position = {:?}", pos);
+
+                if (pos.0 > grid.len() || pos.1 > grid[0].len())
+                {
+                    break;
+                }
+                total += 1;
+            }
+            println!("no can_move, total={}", total);
         }
 
         None => println!("'{}' not found", target),
