d1b417dc2bda1adbd6db0ec5287f71bc35f96469
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2024-12-08 10:50:17 +0000

    Update main.rs

--- src/day_4/main.rs
+++ src/day_4/main.rs
@@ -1,9 +1,41 @@
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 
 // advent of code 2024 : day 4
 // build with: cargo build
 // run with  : cargo run --bin day_4 ./src/day_4/intput.txt
+
+
+
+fn read_grid_from_file(file_path: &str) -> io::Result<Vec<Vec<char>>> {
+    let path = Path::new(file_path);
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut grid = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?; // Read the line as a Result<String>
+        let row: Vec<char> = line.chars().collect(); // Convert each line into Vec<char>
+        grid.push(row);
+    }
+
+    Ok(grid)
+}
+
+fn print_grid(grid: &Vec<Vec<char>>) {
+    for row in 0..grid.len() {
+        for col in 0..grid[row].len() {
+            print!("{}", grid[row][col]);
+        }
+        println!();
+    }
+}
+
+#[warn(dead_code)]
 fn find_in_a_row_forwards(input: &str) -> i32 {
     let mut res: i32 = 0;
 
@@ -23,7 +55,7 @@ fn find_in_a_row_forwards(input: &str) -> i32 {
     }
     res
 }
-
+#[warn(dead_code)]
 fn find_in_a_row_backwards(input: &str) -> i32 {
     let mut res: i32 = 0;
 
@@ -44,21 +76,74 @@ fn find_in_a_row_backwards(input: &str) -> i32 {
     res
 }
 
-fn main() {
+fn find_target(grid: &Vec<Vec<char>>,target:&str,search_direction: (i32,i32)) -> i32
+{
+    let mut total = 0;
+
+    let target_str = target.to_string();
+
+    // this assumes consistent column length
+    println!("target length = {} grid[0].len() {}\n",target.len(),grid[0].len());
+    // search horizontally 
+    if search_direction.0 == 0
+    {
+        // search index increments by 1 for each grid location until found then resets
+        let mut search_index:usize = 0;
+        let search_limit:usize = target.len(); 
+        for row in 0..grid.len() {
+            for col in 0..grid[0].len()  {
+                // the idea is we use the search index to go through and if we get a hit keep going
+                // if not then just reset the search
+                if grid[row][col] != target_str[search_index]  {
+                    search_index = 0;
+                    println!("found {}, search for {}, row {} col {} not found", grid[row][col], target_str[search_index],row, col);
+                } 
+                else
+                {
+                    search_index += 1;
+                    if search_index == search_limit{
+                       println!("FOUND row {} col {} ", row, col);
+                       search_index = 0;
+                       total += 1;
+                    }
+                }
+            }
+        }  
+    }
+
+   
+    total
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>>  {
     // get the input file name from args
     let args: Vec<String> = env::args().collect();
     let input_file: &String = &args[1];
 
-    println!("input file = {input_file}\n");
-    let mut result: i32 = 0;
-    let contents = fs::read_to_string(input_file).expect("error reading file into string");
-
-    for line in contents.lines() {
-        result += find_in_a_row_forwards(line);
-        result += find_in_a_row_backwards(line);
-        println!("{:?}", line);
+    match args.len() <= 1 {
+        true => {
+            panic!("Expected at least one argument, but none were provided.");
+        }
+        false => {
+            println!("input file = {input_file}\n");
+        }
     }
-    // read a vertical column from contents by using offset
-    // todo GIVE UP after 1 hour
-    println!("Result ={}\n", result);
+
+    // read in the file 
+    let grid = read_grid_from_file(input_file)?;
+    
+    // print what we read in
+    print_grid(&grid);
+
+    // find XMAS horizontally vertically and diagonally (forwards and in reverse)
+    
+    // search direction is the offset in row col index to where to look for the next character
+    let search_direction: (i32, i32) = (0,0);
+    let target = "XMAS";
+
+    let mut total = 0;
+    total = find_target(&grid,target,search_direction);
+
+    println!("total = {}\n", total);
+    Ok(())
 }
